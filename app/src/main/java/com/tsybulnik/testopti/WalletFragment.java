5571c16983857da54f7ab151136af0670ca0bb33
Serg8787 <tsybulnikserg@gmail.com>
2021-10-21 15:48:40 +0300

    add basic navigation

--- app/src/main/java/com/tsybulnik/testopti/WalletFragment.java
+++ app/src/main/java/com/tsybulnik/testopti/WalletFragment.java
@@ -1,11 +1,14 @@
 package com.tsybulnik.testopti;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -55,10 +58,13 @@ public class WalletFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        NavController navController = Navigation.findNavController((Activity) getContext(),R.id.navHostFragment);
+
 
        getView().findViewById(R.id.ivAddDeal).setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
+               navController.navigate(R.id.walletAddDealFragment);
 
 
            }
