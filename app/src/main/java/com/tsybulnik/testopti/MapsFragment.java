cfd9dcdf710a5e88c877714986553b3c3e151cae
Serg8787 <tsybulnikserg@gmail.com>
2021-10-23 10:15:11 +0300

    add map

--- app/src/main/java/com/tsybulnik/testopti/MapsFragment.java
+++ app/src/main/java/com/tsybulnik/testopti/MapsFragment.java
@@ -6,8 +6,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,25 +20,64 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsFragment extends Fragment {
-    int lat = 19;
-    int longe = 19;
-
 
 
     private final OnMapReadyCallback callback = new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(19, 73);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient((Activity) getContext());
+            if (ActivityCompat.checkSelfPermission((Activity) getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission((Activity) getContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            fusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if(location!=null){
+                            Geocoder geocoder = new Geocoder((Activity) getContext(), Locale.getDefault());
+                            try {
+                                List<Address> addresses = geocoder.getFromLocation(
+                                        location.getLatitude(),location.getLongitude(),1);
+                                double lat = addresses.get(0).getLatitude();
+                                Log.d("MyLog",""+lat);
+                                double longe = addresses.get(0).getLongitude();
+                                Log.d("MyLog",""+longe);
+
+                                LatLng sydney = new LatLng(lat, longe);
+                                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                }
+            });
         }
     };
 
@@ -51,6 +93,9 @@ public class MapsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+
+
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
@@ -58,4 +103,6 @@ public class MapsFragment extends Fragment {
         }
 
     }
+
+
 }
